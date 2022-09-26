34d265d4ef4dbb012fb81045c26d116d31a150aa
Hemavardhan03 <112305810+Hemavardhan03@users.noreply.github.com>
2022-09-26 09:55:08 +0530

    Update .si.py

--- .si.py
+++ .si.py
@@ -2,4 +2,4 @@ principal=float(input("enter the principal amount"))
 time=float(input("enter the time period"))
 rate=float(input("enter the rate"))
 simple_interest=(principal*time*rate)/100
-print("simple interest:",simple_interest)995
+print("simple interest:",simple_interest)
