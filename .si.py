3ad8be59a3f52eaf60ad1946648c3cf5ed523016
Hemavardhan03 <112305810+Hemavardhan03@users.noreply.github.com>
2022-09-26 09:54:32 +0530

    Add files via upload

---
+++ .si.py
@@ -0,0 +1,5 @@
+principal=float(input("enter the principal amount"))
+time=float(input("enter the time period"))
+rate=float(input("enter the rate"))
+simple_interest=(principal*time*rate)/100
+print("simple interest:",simple_interest)995
