f9b0d5aa89aebdd3fb606663b53279d45665ba85
Hemavardhan03 <112305810+Hemavardhan03@users.noreply.github.com>
2022-09-30 11:45:48 +0530

    Add files via upload

---
+++ printing vowels and string.py
@@ -0,0 +1,21 @@
+def Check_Vow(string, vowels):
+
+    string = string.casefold()
+
+    count = {}.fromkeys(vowels, 0)
+
+    for character in string:
+
+        if character in count:
+
+            count[character] += 1
+
+    return count
+
+vowels = 'aeiou'
+
+string = "saveetha school of engeneering"
+
+print (Check_Vow(string, vowels))
+
+
